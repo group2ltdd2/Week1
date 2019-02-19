ebec740df9f13b8c2204583e790d9041fa0e41bc
group2ltdd2 <47730741+group2ltdd2@users.noreply.github.com>
2019-02-19 20:50:55 +0700

    Add files via upload

---
+++ Media/main/java/vn/edu/tdc/music/Activity_music.java
@@ -0,0 +1,13 @@
+package vn.edu.tdc.music;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_music extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_music);
+    }
+}
